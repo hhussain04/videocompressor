c44fbb0660242a23781cfbbf885258645a69575d
Grid <147248100+hhussain04@users.noreply.github.com>
2025-02-05 22:03:19 +0000

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  compressFile: (filePath, options) => ipcRenderer.invoke('compress-file', { filePath, options })
-})
